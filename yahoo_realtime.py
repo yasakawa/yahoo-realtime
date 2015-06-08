d1d9b65e86fc4d69e4c06e84db6512ae04d1d083
yasakawa <yos.asakawa@gmail.com>
2015-06-08 22:59:25 +0900

    Add tag parse logic.

--- yahoo_realtime.py
+++ yahoo_realtime.py
@@ -1,17 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import re
 import urllib
 import urllib2
 import logging
 from bs4 import BeautifulSoup
 
-def search(keyword, b=None):
+def search(keyword):
     keyword_enc = urllib.quote(keyword)
-    if b:
-        url_string = "http://realtime.search.yahoo.co.jp/search?p=%s&b=%d" % (keyword, b)
-    else:
-        url_string = "http://realtime.search.yahoo.co.jp/search?p=%s&ei=UTF-8" % (keyword)
+    url_string = "http://realtime.search.yahoo.co.jp/search?p=%s&ei=UTF-8" % (keyword)
         
     useragent = "Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; Touch; rv:11.0) like Gecko"
     logging.info("URL: %s" % url_string)
@@ -37,9 +35,29 @@ if __name__ == '__main__':
     argvs = sys.argv  # コマンドライン引数を格納したリストの取得
     argc = len(argvs) # 引数の個数
 
-    html = search('CYBR', 10)
-    soup = BeautifulSoup(html)
-    h2_soup = soup.find_all('h2')
+    reg = re.compile(r"http%3a//twitter.com/([A-Za-z0-9_]+)/status/([0-9]+)")
 
+    html = search('CYBR')
+    soup = BeautifulSoup(html)
+    
+    tweets = []
+    
+    cnf_soups = soup.find_all('div', class_="cnt" )
+    for cnf_soup in cnf_soups:
+        tweet = {}
+        tweet['data_time'] = cnf_soup['data-time']
+
+        h2_soup = cnf_soup.find('h2')
+        tweet['text'] = h2_soup.get_text(strip=True)
+        
+        inf_soups = cnf_soup.select('a[title]')
+        if inf_soups:
+            href = inf_soups[0]['href']
+            match = reg.search(href)            
+
+            tweet['screen_name'] = match.group(1)
+            tweet['tweet_id_str'] = match.group(2)
+            tweets.append(tweet)
+        
     import pprint
-    pprint.pprint(h2_soup)
+    pprint.pprint(tweets)
